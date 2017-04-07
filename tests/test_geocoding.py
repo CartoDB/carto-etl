a3550a2921e42f9b299af2f28e6cf039dbf11921
Alberto Romeu <alrocar@cartodb.com>
2017-04-07 11:35:50 -0400

    fixed test_geocoding.py for a sample.csv

--- tests/test_geocoding.py
+++ tests/test_geocoding.py
@@ -1,8 +1,11 @@
 import pytest
+import os
 
 from geocoding import *
 
 def test_error():
-    request_id = 'g109VcAbAFutjv18y9a1blWYQ7JexZGv'
-    geocoding_job = HereGeocodingJob(csv_file_path='/Users/alrocar/Documents/CartoDB/carto-etl/test_files/sample_bbva.csv', email='alrocar@cartodb.com', request_id=request_id)
+    #request_id = 'g109VcAbAFutjv18y9a1blWYQ7JexZGv'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    request_id = None
+    geocoding_job = HereGeocodingJob(csv_file_path=dir_path + '/../test_files/sample.csv', email='alrocar@cartodb.com', request_id=request_id)
     geocoding_job.download()
